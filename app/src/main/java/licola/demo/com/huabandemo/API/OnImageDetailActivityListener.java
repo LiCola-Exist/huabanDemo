0904febd34ffc075024ce915bcf2ffb10f8d913e
LiCola <361401376@qq.com>
2016-05-11 14:51:23 +0800

    delete

--- app/src/main/java/licola/demo/com/huabandemo/API/OnImageDetailActivityListener.java
+++
@@ -1,9 +0,0 @@
-package licola.demo.com.huabandemo.API;
-
-/**
- * Created by LiCola on  2016/04/23  17:02
- */
-public interface OnImageDetailActivityListener {
-    void onLikeChange(boolean isLikeChange);
-    void onGatherChange(boolean isGatherChange);
-}
